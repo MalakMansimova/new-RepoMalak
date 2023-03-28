28a4968891915556ff9c5e987627da0e54ecd3b8
MalakMansimova <126595626+MalakMansimova@users.noreply.github.com>
2023-03-28 11:57:37 +0400

    Commit

---
+++ To Do(M)/assets/main.js
@@ -0,0 +1,9 @@
+const input=document.querselector('input')
+const div=document.getElementById('todo')
+const btn=docuent.querselector('button')
+btn.addEventListener('click',function(alt){
+
+   
+console.log('Salam')
+
+})
